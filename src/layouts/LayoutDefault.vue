a33ee65f53db9c30b0fbac0fc5182a5c6785c6d9
Carolina Ribeiro <carolinaribeirodacol@gmail.com>
2026-01-02 00:44:02 -0300

    feat: :white_check_mark: Configurando Layout e Pages

--- src/layouts/LayoutDefault.vue
+++ src/layouts/LayoutDefault.vue
@@ -1,9 +1,11 @@
 <script setup>
+import NavBar from '@/components/NavBar.vue'
 import { RouterView } from 'vue-router'
 </script>
 
 <template>
   <div class="h-screen">
+    <NavBar />
     <RouterView />
   </div>
 </template>
--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,13 +1,5 @@
 <script setup></script>
 
 <template>
-  <div>
-    Home
-
-    <button class="btn btn-neutral">Neutral</button>
-    <button class="btn btn-primary">Primary</button>
-    <button class="btn btn-secondary">Secondary</button>
-    <button class="btn btn-accent">Accent</button>
-    <button class="btn btn-info">Info</button>
-  </div>
+  <div class="p-4">Home</div>
 </template>
