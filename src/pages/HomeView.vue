8dd3ee47f7de49f4f7abea8d85c710d7bc571d42
Carolina Ribeiro <carolinaribeirodacol@gmail.com>
2026-01-02 01:24:45 -0300

    feat: Auto-import unplugin-auto-import

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,5 +1,11 @@
-<script setup></script>
+<script setup>
+const text = ref('')
+const textWithComputed = computed(() => text.value)
+</script>
 
 <template>
-  <div class="p-4">Home</div>
+  <div class="p-4">
+    Home <input v-model="text" type="text" placeholder="Type here" class="input" />
+    {{ textWithComputed }}
+  </div>
 </template>
--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,28 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    tailwindcss(),
+    AutoImport({
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+
+        /\.vue$/, // .vue
+
+        /\.vue\?vue/, // .vue?vue
+
+        /\.md$/, // .md
+      ],
+
+      imports: ['vue'],
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
