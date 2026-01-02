406dbf1c889cf11a6453efd1e65183253402a35b
Carolina Ribeiro <carolinaribeirodacol@gmail.com>
2026-01-02 00:22:55 -0300

    feat: Configurando Projeto com DaisyUI

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,5 +1,13 @@
 <script setup></script>
 
 <template>
-  <div>Home</div>
+  <div>
+    Home
+
+    <button class="btn btn-neutral">Neutral</button>
+    <button class="btn btn-primary">Primary</button>
+    <button class="btn btn-secondary">Secondary</button>
+    <button class="btn btn-accent">Accent</button>
+    <button class="btn btn-info">Info</button>
+  </div>
 </template>
