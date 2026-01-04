c2e96472156bcd0620f1462d9501fa3799122f07
Carolina Ribeiro <carolinaribeirodacol@gmail.com>
2026-01-04 01:36:20 -0300

    feat: paginação

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -5,13 +5,21 @@ import { onMounted, ref } from 'vue'
 
 const episodes = ref([])
 
+const page = ref(1)
+const totalPages = ref(0)
+
 onMounted(() => {
   handleGetEpisodes()
 })
 
 async function handleGetEpisodes() {
   try {
-    const response = await axios.get('https://rickandmortyapi.com/api/episode')
+    const response = await axios.get('https://rickandmortyapi.com/api/episode', {
+      params: {
+        page: page.value,
+        totalPages: response.data.info.pages,
+      },
+    })
 
     episodes.value = response.data.results
 
@@ -20,11 +28,18 @@ async function handleGetEpisodes() {
     console.log(error)
   }
 }
+
+function setPagination(newPage) {
+  page.value = newPage
+
+  handleGetEpisodes()
+}
 </script>
 
 <template>
   <div class="p-4">
-    Home
+    <h1>Episodes</h1>
+
     <div class="grid grid-cols-1 md:grid-cols-3 mg:grid-cols-3 gap-6">
       <BaseCard
         v-for="episode in episodes"
@@ -35,7 +50,19 @@ async function handleGetEpisodes() {
         :airDate="episode.air_date"
       />
 
-      {{ episodes }}
+      <div class="flex justify-center py-6">
+        <div class="join">
+          <button
+            v-for="(currentPage, index) in totalPages"
+            :key="index"
+            class="join-item btn btn-lg md:btn-md"
+            :class="{ 'btn-active': index + 1 === currentPage }"
+            @click="setPagination(index + 1)"
+          >
+            {{ index + 1 }}
+          </button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
