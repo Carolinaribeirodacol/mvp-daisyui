ef522292d3b7e26016e496c765edaa6b34a1ff48
Carolina Ribeiro <carolinaribeirodacol@gmail.com>
2026-01-06 03:48:45 -0300

    feat: mostrando cada personagem ao clicar

--- src/components/ModalEpisode.vue
+++ src/components/ModalEpisode.vue
@@ -44,37 +44,130 @@ function getImage(url) {
 
 function closeModal() {
   show.value = false
+  selectedCharacter.value = null
+}
+
+const selectedCharacter = ref(null)
+
+async function handleGetCharacter(characterUrl: string) {
+  const characterId = characterUrl.split('/').at(-1)
+
+  try {
+    const response = await axios.get(`https://rickandmortyapi.com/api/character/${characterId}`)
+
+    selectedCharacter.value = response.data
+  } catch (error) {
+    console.log(error)
+  }
 }
 </script>
 
 <template>
   <dialog class="modal" :class="{ 'modal-open': show }">
-    <div class="modal-box">
+    <div class="modal-box w-11/12 max-w-2xl">
       <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2" @click="closeModal">
         ✕
       </button>
 
-      <template v-if="episode">
-        <h3 class="text-xl text-center text-secondary font-semibold pb-6">
-          Episode <Badge background="secondary" color="base-100">{{ episode.episode }}</Badge>
-        </h3>
+      <div v-if="episode">
+        <div class="card-body p-0">
+          <h3 class="card-title justify-center text-secondary">
+            Episode
+            <Badge background="secondary" color="base-100">
+              {{ episode.episode }}
+            </Badge>
+          </h3>
 
-        <div class="flex flex-col gap-4">
-          <h3 class="text-primary"><span class="font-semibold">Name: </span>{{ episode.name }}</h3>
+          <div class="card bg-base-100 shadow mb-6">
+            <div class="stats stats-vertical shadow">
+              <div class="stat">
+                <div class="stat-title text-primary">Name</div>
+                <div class="stat-value text-base text-neutral">
+                  {{ episode.name }}
+                </div>
+              </div>
 
-          <div>
-            <p class="text-primary pb-2 font-semibold">Characters:</p>
+              <div class="stat">
+                <div class="stat-title text-primary">Air date</div>
+                <div class="stat-value text-base text-neutral">
+                  {{ episode.air_date }}
+                </div>
+              </div>
 
-            <div class="avatar p-1" v-for="(character, index) in episode.characters" :key="index">
-              <div class="w-16 rounded-full">
-                <img :src="getImage(character)" />
+              <div class="stat">
+                <div class="stat-title text-primary">Total of characters</div>
+                <div class="stat-value text-base text-neutral">
+                  {{ episode.characters.length }}
+                </div>
+              </div>
+
+              <div class="stat">
+                <div class="stat-title text-primary">Character List</div>
+                <div class="stat-value text-base text-neutral flex flex-wrap">
+                  <div
+                    v-for="(character, index) in episode.characters"
+                    :key="index"
+                    class="avatar p-1"
+                  >
+                    <div
+                      class="w-16 rounded-full hover:cursor-pointer transition-transform duration-300 hover:scale-110"
+                      @click="handleGetCharacter(character)"
+                    >
+                      <img :src="getImage(character)" />
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-
-          <Badge outline>Air date: {{ episode.air_date }}</Badge>
         </div>
-      </template>
+
+        <div v-if="selectedCharacter" class="card bg-base-100 shadow">
+          <div class="card-body p-0">
+            <h3 class="card-title justify-center text-secondary">Character Info</h3>
+
+            <div class="flex justify-center">
+              <div class="avatar">
+                <div
+                  class="w-28 rounded-full ring ring-secondary ring-offset-base-100 ring-offset-2"
+                >
+                  <img :src="getImage(selectedCharacter.url)" />
+                </div>
+              </div>
+            </div>
+
+            <div class="stats stats-vertical md:stats-horizontal shadow">
+              <div class="stat">
+                <div class="stat-title text-primary">Name</div>
+                <div class="stat-value text-base text-neutral">
+                  {{ selectedCharacter.name }}
+                </div>
+              </div>
+
+              <div class="stat">
+                <div class="stat-title text-primary">Status</div>
+                <div class="stat-value text-base text-neutral">
+                  {{ selectedCharacter.status }}
+                </div>
+              </div>
+
+              <div class="stat">
+                <div class="stat-title text-primary">Species</div>
+                <div class="stat-value text-base text-neutral">
+                  {{ selectedCharacter.species }}
+                </div>
+              </div>
+
+              <div class="stat">
+                <div class="stat-title text-primary">Gender</div>
+                <div class="stat-value text-base text-neutral">
+                  {{ selectedCharacter.gender }}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
       <div v-else class="flex justify-center">
         <span class="loading loading-dots loading-md"></span>
